857ba0a9e8d2df56b480293b77f5a4341c6b3d0f
Hussen Mugumbate <mhussayn95@gmail.com>
2023-02-16 10:23:19 -0800

    6 size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ float  f;
 printf("size of char:%lu bytes(s)\n", (unsigned long)sizeof(a));
 printf("size of int:%lu bytes(s)\n", (unsigned long)sizeof(b));
 printf("size of long int:%lu bytes(s)\n", (unsigned long)sizeof(f));
-printf("size of long long int:%lu bytes(s)\n", (unsigned long)sizeof(c));
+printf("size of long long int:%lu bytes(s)\n", (unsigned long)sizeof(d));
 printf("size of float:%lu bytes(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
