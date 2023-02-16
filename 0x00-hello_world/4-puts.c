bdcf555314359a6897da0021dc146ded5aea6816
Hussen Mugumbate <mhussayn95@gmail.com>
2023-02-16 08:35:35 -0800

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
- *Main - A program that prints with puts function
- *Return: 0(success)
+ * Main - A program that prints with puts function
+ * Return: 0(success)
  */
 int main(void)
 {
