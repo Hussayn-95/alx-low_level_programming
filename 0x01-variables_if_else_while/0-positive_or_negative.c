fde8cc4523f2559b976ec7c1170e453e78c228d9
Hussen Mugumbate <mhussayn95@gmail.com>
2023-02-17 03:00:22 -0800

    if else

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - if else function
+ * Return: 0 (always)
+ */
+int main(void)
+{
+	int x = 0;
+
+	if (x < 0)
+	{
+		printf("is positive\n");
+	}
+	else if (x == 0)
+	{
+		printf("is zero\n");
+	}
+       	else if (x > 0)
+	{
+		printf("is negative\n");
+	}
+	return (0);
+}
