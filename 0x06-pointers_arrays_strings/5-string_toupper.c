f8f0fd9d6eb46ff352d6bf72c06e4b5901cbad0a
Hussen Mugumbate <mhussayn95@gmail.com>
2023-03-02 10:09:01 -0800

    string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - relacing lowercase to uppercase
+ * @m: input 
+ * Return: m
+ */
+char *string_toupper(char *m)
+{
+	int i;
+
+	for (i = 0; m[i] != '\0'; i++)
+	{
+		if (m[i] >= 'a' && m[i] < 'Z')
+			m[i] =(int)m[i] - 32;
+	}
+	return (m);
+}
