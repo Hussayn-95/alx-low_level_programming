6ad251dff15e9ad1d0a31e01f3cbdd386b783707
Hussen Mugumbate <mhussayn95@gmail.com>
2023-03-02 06:04:56 -0800

    Adding two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcat - combine two strings
+ * @s: input
+ * return: always 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j = strlen[s];
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		s[i +  j] = s1[i];
+	}
+	s[i + j} = '\0';
+	return (0);
+
+}
