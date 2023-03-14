d17320ca302c3a5d51fa7f61d295c3ef0085342a
Hussen Mugumbate <mhussayn95@gmail.com>
2023-03-14 05:28:35 -0700

    argv and argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>    
+#include "main.h"
+                         
+/**
+ * main -  main function
+ * @argv - prints the vector of arrays
+ * @argc - refers to the arguments count
+ * Return: 0 success
+ */
+int main(int argc, char* argv[])
+{
+	(void) argc;
+
+	print("argv = %s\n", argv[0]);
+
+	return (0);
+}
